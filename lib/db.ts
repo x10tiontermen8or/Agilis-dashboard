b63a0c717f6196a9253837ea878f45a797e51f6f
GAURAV <glitzgauravxo@gmail.com>
2025-09-26 11:51:45 +0530

    Fix project setup

--- lib/db.ts
+++ lib/db.ts
@@ -14,4 +14,7 @@ const prisma = globalThis.prisma ?? prismaClientSingleton()
 
 export default prisma
 
-if (process.env.NODE_ENV !== 'production') globalThis.prisma = prisma
+// This line checks if we are on the server before trying to use `process`
+if (typeof process !== 'undefined' && process.env.NODE_ENV !== 'production') {
+  globalThis.prisma = prisma
+}
